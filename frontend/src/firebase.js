1c61d2b87f27c9e474cc1c57bf58073d4f30b7da
Mia Strinic <mstrin01@fesb.hr>
2025-05-21 17:11:48 +0200

    firebase configuration changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2ZIXVIDINZlipZ536vHnCB9YplyqpjOg",
-    authDomain: "hcr1-de7e7.firebaseapp.com",
-    projectId: "hcr1-de7e7",
-    storageBucket: "hcr1-de7e7.firebasestorage.app",
-    messagingSenderId: "521660056183",
-    appId: "1:521660056183:web:7e98c58f6a46ac390b7c72",
-    measurementId: "G-673YGZXPKK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
